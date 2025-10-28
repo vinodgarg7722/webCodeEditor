a1050599290e7a5c67555ed9e6cacb3c60486ca2
vinod garg <vinodgarg070702@gmail.com>
2025-10-28 10:51:25 +0530

    08-playground-file-explorer-1

---
+++ lib/template.ts
@@ -0,0 +1,10 @@
+export const templatePaths = {
+  REACT: "/vibecode-starters/react-ts",
+  NEXTJS: "/vibecode-starters/nextjs-new",
+  EXPRESS: "/vibecode-starters/express-simple",
+  VUE: "/vibecode-starters/vue",
+  HONO: "/vibecode-starters/hono-nodejs-starter",
+  ANGULAR: "/vibecode-starters/angular",
+};
+
+
